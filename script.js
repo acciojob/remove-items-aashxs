88c5fc835d7164d756a5a0f50920a0becde722e1
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-02 12:48:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let select=document.querySelector("#colorSelect");
+let btn=document.querySelector("input");
+
+
+function deleteOption() {
+    // Get the selected option
+    let selectedOption = select.value;
+
+    // Find the option in the dropdown and remove it
+    for(let i=0; i<select.options.length; i++) {
+        if(select.options[i].value == selectedOption) {
+            select.remove(i);
+            break;
+        }
+    }
+}
+
+btn.addEventListener("click", deleteOption);
